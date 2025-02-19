c4e8a803efa0cfc302e1a0779a2661ccb2fc96f4
adams <adams@HarrisonPC.dhcp.wustl.edu>
2025-02-19 13:53:01 -0600

    We made a flag.

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,16 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		 StdDraw.setXscale(0, 1);
+	     StdDraw.setYscale(0, 1);
 		
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledRectangle(1.0/6, 0.5, 1.0/6, 0.5);
+		
+		StdDraw.setPenColor(StdDraw.GREEN);
+		StdDraw.filledRectangle(0.5, 0.5, 1.0/6, 0.5);
+		
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledRectangle(5.0/6, 0.5, 1.0/6, 0.5);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,77 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		StdDraw.setPenColor(red, green, blue);
+		if (isFilled) {
+			if ("ellipse".equals(shape)) {
+				double x1 = in.nextDouble();
+				double y1 = in.nextDouble();
+				double x2 = in.nextDouble();
+				double y2 = in.nextDouble();
+				StdDraw.filledEllipse(x1, y1, x2, y2);
+			}
+			
+			if ("rectangle".equals(shape)) {
+				double x1 = in.nextDouble();
+				double y1 = in.nextDouble();
+				double x2 = in.nextDouble();
+				double y2 = in.nextDouble();
+				StdDraw.filledRectangle(x1, y1, x2, y2);
+			}
+			
+			if ("triangle".equals(shape)) {
+				double x1 = in.nextDouble();
+				double y1 = in.nextDouble();
+				double x2 = in.nextDouble();
+				double y2 = in.nextDouble();
+				double x3 = in.nextDouble();
+				double y3 = in.nextDouble();
+				double[] x = {x1, x2, x3};
+				double[] y = {y1, y2, y3};
+				StdDraw.filledPolygon(x, y);
+			}
+		}
+		else {
+			if ("ellipse".equals(shape)) {
+				double x1 = in.nextDouble();
+				double y1 = in.nextDouble();
+				double x2 = in.nextDouble();
+				double y2 = in.nextDouble();
+				StdDraw.ellipse(x1, y1, x2, y2);
+			}
+			
+			if ("rectangle".equals(shape)) {
+				double x1 = in.nextDouble();
+				double y1 = in.nextDouble();
+				double x2 = in.nextDouble();
+				double y2 = in.nextDouble();
+				StdDraw.rectangle(x1, y1, x2, y2);
+			}
+			
+			if ("triangle".equals(shape)) {
+				double x1 = in.nextDouble();
+				double y1 = in.nextDouble();
+				double x2 = in.nextDouble();
+				double y2 = in.nextDouble();
+				double x3 = in.nextDouble();
+				double y3 = in.nextDouble();
+				double[] x = {x1, x2, x3};
+				double[] y = {y1, y2, y3};
+				StdDraw.polygon(x, y);
+			}
+		}
 		
+		double x1;
+		double y1;
+		double x2;
+		double y2;
+		double x3;
+		double y3;
+		StdDraw.rectangle(0, 0, 0, 0);
 	}
 }
